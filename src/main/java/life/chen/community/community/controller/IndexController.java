670f3d15e073f9e54c6eb3efb16a2496a229bfad
bin546882521 <546882521@qq.com>
2021-01-20 13:41:18 +0800

    add index and bootstrap

---
+++ src/main/java/life/chen/community/community/controller/IndexController.java
@@ -0,0 +1,17 @@
+package life.chen.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+//    @GetMapping("/hello")
+//    public String hello(@RequestParam(name = "name") String name, Model model){
+//        model.addAttribute("name", name);
+//        return "hello";
+//    }
+}
