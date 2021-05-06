14d6998c9f0855bf73b81803a2e2b4e904c70b3f
bin546882521 <546882521@qq.com>
2021-05-06 15:19:50 +0800

    12345

--- src/main/resources/db/migration/V3__Create_question_table.sql
+++ src/main/resources/db/migration/V3__Create_question_table.sql
@@ -2,7 +2,7 @@ create table question
 (
     id int auto_increment primary key,
     title varchar(50),
-    description text,
+    description varchar(1024),
     gmt_create bigint,
     gmt_modified bigint,
     creator int,
